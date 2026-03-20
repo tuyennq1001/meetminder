4fbe05879f902366183367361a21ca9ed7ef8aae
Terry Nguyen <tuyen@relipasoft.com>
2026-03-20 22:38:25 +0700

    test: bump to v0.5.1 for auto-update test

--- src/js/app.js
+++ src/js/app.js
@@ -76,7 +76,7 @@ class App {
         // Check for updates (non-blocking)
         this._checkForUpdates();
 
-        console.log('🌐 My Translator v0.5.0 initialized');
+        console.log('🌐 My Translator v0.5.1 initialized');
     }
 
     async _checkPlatformSupport() {
